5544e8d043eff88c0e0633f93d88f7f2cc51bb0e
BANO.notIT <bano.notit@yandex.ru>
2019-11-17 18:00:32 +0300

    core: get architecture from Shieldy sources

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,37 @@
-import Knex from 'knex'
-import {Model} from "objection";
-import Team from "./models/Team";
+// Dependencies
+import { bot } from './helpers/bot';
+import { setupSession } from './helpers/session';
+import Knex from 'knex';
+import { Model } from 'objection';
+import setupStageAndStart from './helpers/stage';
+import Team from './models/Team';
 
 
-const knex = Knex({
-    ...require('../knexfile.js')[process.env.NODE_ENV || 'development']
-});
-
-
-Model.knex(knex);
-
-
-async function main() {
-
-
-    let b = await Team.query().findByIds([1, 2, 3]);
-    console.log(b);
+async function setupDb() {
+    const knex = Knex({
+        ...require('../knexfile.js')[process.env.NODE_ENV || 'development']
+    });
+    Model.knex(knex);
+    await knex.migrate.latest();
 }
 
+async function setupBot() {
+    bot.catch(console.log);
+
+    setupSession(bot);
+    setupStageAndStart(bot);
+
+    bot.startPolling();
+}
+
+async function setupFirstTeam() {
+    const team = await Team.query().findById(1);
+
+    console.log(`Orgs team invite link: t.me/itss_docs_bot?start=${team.inviteToken}`);
+}
 
 Promise.resolve()
-    .then(() => knex.migrate.latest())
-    .then(() => main())
+    .then(setupDb)
+    .then(setupBot)
+    .then(setupFirstTeam)
     .catch(console.error);
-
-console.log(Team);
