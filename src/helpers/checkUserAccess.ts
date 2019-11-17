b5b1593b50997aa077574b0aa3d6df6114a9c676
BANO.notIT <bano.notit@yandex.ru>
2019-11-18 01:29:58 +0300

    feat(db): add teams.isAdmin and checkUserAccess util

---
+++ src/helpers/checkUserAccess.ts
@@ -0,0 +1,12 @@
+import { ContextMessageUpdate, Middleware } from 'telegraf';
+import { __ } from './strings';
+
+const middleware: Middleware<ContextMessageUpdate> = async (ctx, next) => {
+    if (ctx.user?.team.isAdmin) {
+        return next();
+    }
+
+    await ctx.reply(__('system.accessDenied'));
+    await ctx.scene.leave();
+};
+export default middleware;
