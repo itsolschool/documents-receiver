f7c6a4d946c951c9313c17656ccbcceffd6fa409
BANO.notIT <bano.notit@yandex.ru>
2019-11-17 18:06:35 +0300

    db: fix add token migration

--- migrations/20191117162112_add token.js
+++ migrations/20191117162112_add token.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
-    return knex.schema.table('teams', (t) => {
+    return knex.schema.alterTable('teams', (t) => {
         t.string('inviteToken', 10).nullable();
     });
 };
 
 exports.down = function(knex) {
-    return knex.schema.table('teams', (t) => {
+    return knex.schema.alterTable('teams', (t) => {
         t.dropColumn('inviteToken');
     });
 };
