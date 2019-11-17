8e8bf65c5bed3f8dacb5b01dc05f707b0a71abb3
BANO.notIT <bano.notit@yandex.ru>
2019-11-17 18:44:49 +0300

    feat: set default scene to main

--- src/helpers/stage.ts
+++ src/helpers/stage.ts
@@ -3,7 +3,7 @@ import referral from '../scenes/referral';
 import main from '../scenes/main';
 
 export default function setupStage<T extends ContextMessageUpdate>(bot: Telegraf<T>) {
-    const stage = new Stage([referral, main]);
+    const stage = new Stage([referral, main], { default: 'main' });
 
     bot.use(stage.middleware()).command('start', async (ctx) => {
         await ctx.scene.enter('referral');
