9ddd8bfc8b8afea2267bc5fc398bee76dfd56745
BANO.notIT <bano.notit@yandex.ru>
2019-11-20 21:55:52 +0300

    core: add TrelloService to context

---
+++ src/helpers/trello.ts
@@ -0,0 +1,16 @@
+import TrelloService from '../services/TrelloService';
+import Telegraf, { ContextMessageUpdate } from 'telegraf';
+import AppVar, { APP_TRELLO_TOKEN_KEY } from '../models/AppVar';
+
+
+export async function setupTrello<T extends ContextMessageUpdate>(bot: Telegraf<T>) {
+    let token = await AppVar.query().findById(APP_TRELLO_TOKEN_KEY);
+    const service = new TrelloService(token?.value);
+
+    bot.use(((ctx, next) => {
+        ctx.trello = service;
+        return next();
+    }));
+
+    return service;
+}
