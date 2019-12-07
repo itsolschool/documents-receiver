77de20ae2fb803a52c92cf44d74647a432dea6ce
BANO.notIT <bano.notit@yandex.ru>
2019-12-08 00:57:43 +0300

    refactor: Referral scene into middleware

--- src/helpers/stage.ts
+++ src/helpers/stage.ts
@@ -1,13 +1,12 @@
 import Telegraf, { ContextMessageUpdate, Stage } from 'telegraf'
 import { SCENE } from '../const/sceneId'
-import referral from '../scenes/referral'
 import main from '../scenes/main'
 import gdrive from '../scenes/gdrive'
 import addTeam from '../scenes/addTeam'
 import uploadDocument from '../scenes/uploadDocument'
 
 export function setupStage<T extends ContextMessageUpdate>(bot: Telegraf<T>) {
-    const stage = new Stage([gdrive, referral, main, addTeam, uploadDocument], { default: SCENE.MAIN })
+    const stage = new Stage([gdrive, main, addTeam, uploadDocument], { default: SCENE.MAIN })
 
     bot.use(stage.middleware())
 }
