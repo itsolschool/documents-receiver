a0fc461d43853579c46315e00aa57c9d766ce272
BANO.notIT <bano.notit@yandex.ru>
2019-12-08 01:04:02 +0300

    feat(scene): make main scene texts

--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -1,20 +1,16 @@
-import { BaseScene, ContextMessageUpdate, Stage } from 'telegraf'
+import { BaseScene, ContextMessageUpdate, Markup, Stage } from 'telegraf'
 import checkUserIsAdmin from '../middlewares/checkUserIsAdmin'
 import { GREEN_MARK, RED_CROSS, WHITE_QUESTION_MARK } from '../const/emojies'
 import { SCENE } from '../const/sceneId'
+import { __ } from '../helpers/strings'
 
 const scene = new BaseScene<ContextMessageUpdate>(SCENE.MAIN)
 scene
-    .enter((ctx) => {
-        ctx.reply('main start')
-    })
-    .command('test', async (ctx) => {
-        await ctx.reply('tester command ' + ctx.user?.fullName)
-    })
-    .command('upload', Stage.enter(SCENE.UPLOAD_DOCUMENT))
-    .command('gdrive', checkUserIsAdmin, Stage.enter(SCENE.GDRIVE_SETUP))
-    .command('addTeam', checkUserIsAdmin, Stage.enter(SCENE.TEAM_ADD))
-    .command('check', async (ctx) => {
+    .enter(replyWithMainView)
+    .hears(__('main.btns.uploadDocuments'), Stage.enter(SCENE.UPLOAD_DOCUMENT))
+    .hears(__('main.btns.addTeam'), checkUserIsAdmin, Stage.enter(SCENE.TEAM_ADD))
+    .hears(__('main.btns.setupGDrive'), checkUserIsAdmin, Stage.enter(SCENE.GDRIVE_SETUP))
+    .hears(__('main.btns.healthcheck'), async (ctx) => {
         const message = await ctx.reply(`${WHITE_QUESTION_MARK} GDrive checking...`)
         try {
             await ctx.gdrive.checkOperational()
@@ -33,4 +29,26 @@ scene
             )
         }
     })
+    .use(replyWithMainView)
+
+const ADMIN_MARKUP = Markup.keyboard([
+    [__('main.btns.addTeam'), __('main.btns.setupGDrive')],
+    [__('main.btns.healthcheck')]
+])
+    .oneTime(true)
+    .resize(true)
+    .extra()
+const USER_MARKUP = Markup.keyboard([[__('main.btns.uploadDocuments')]])
+    .oneTime(true)
+    .resize(true)
+    .extra()
+
+async function replyWithMainView(ctx: ContextMessageUpdate) {
+    if (ctx.user.team.isAdmin) {
+        await ctx.reply(__('main.admin'), ADMIN_MARKUP)
+    } else {
+        await ctx.reply(__('main.user'), USER_MARKUP)
+    }
+}
+
 export default scene
