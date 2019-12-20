2d7454fc6bdfd560a0e85db8db33f1252bf80b15
BANO.notIT <bano.notit@yandex.ru>
2019-12-20 05:24:14 +0300

    build+feat: add sentry error logging

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import Knex from 'knex'
 import { Model } from 'objection'
-import attachUser from './middlewares/attachUser'
+import * as path from 'path'
+import { BotConfig } from 'telegraf'
+import Sentry from '@sentry/node'
 
+import attachUser from './middlewares/attachUser'
 import { bot } from './helpers/bot'
 import { bindRedisSession } from './helpers/redisSession'
 import { setupStage } from './helpers/stage'
-import { BotConfig } from 'telegraf'
 import { bindGDrive } from './helpers/gdrive'
 import { bindTrello } from './helpers/trello'
-import * as path from 'path'
 import afterStart from './helpers/afterStart'
 import bindConfig from './helpers/bindConfig'
 import { setupReferralMiddleware } from './middlewares/referralMiddleware'
@@ -16,6 +17,8 @@ import { setupReferralMiddleware } from './middlewares/referralMiddleware'
 const SECRET_WEBHOOK_PATH = process.env.WEBHOOK_PATH
 const config: BotConfig = require(path.resolve(__dirname, '../config/general.json'))
 const debug = require('debug')('bot')
+
+Sentry.init({ dsn: 'https://50ab52136ccb41f88330c6c4b096eab8@sentry.io/1862805' })
 
 async function setupDb() {
     const knex = Knex({
@@ -74,4 +77,3 @@ Promise.resolve()
     .then(setupDb)
     .then(setupBot)
     .then(afterStart)
-    .catch(console.error)
