6eb163f39e79b0197a9a3272a26a843499f6117f
BANO.notIT <bano.notit@yandex.ru>
2019-12-20 06:15:13 +0300

    feat: add transaction into gdrive scene

--- src/scenes/gdrive.ts
+++ src/scenes/gdrive.ts
@@ -4,6 +4,7 @@ import AppVar, { APP_VAR_KEYS } from '../models/AppVar'
 import { SCENE } from '../const/sceneId'
 import { ExtraReplyMessage } from 'telegraf/typings/telegram-types'
 import { CancelableScene } from '../helpers/CancelableScene'
+import { transaction } from 'objection'
 
 const scene = new CancelableScene<ContextMessageUpdate>(SCENE.GDRIVE_SETUP)
 scene
@@ -14,18 +15,19 @@ scene
         await ctx.reply(__('gdrive.askForToken'), keyboard.extra())
     })
     .on('text', async (ctx) => {
-        await ctx.reply(__('gdrive.ok'))
         try {
             const creds = await ctx.gdrive.getCredentialsByCode(ctx.message.text)
-            await AppVar.query().deleteById(APP_VAR_KEYS.GDRIVE_ACCESS_TOKEN)
-            await AppVar.query().insert({
-                key: APP_VAR_KEYS.GDRIVE_ACCESS_TOKEN,
-                value: JSON.stringify(creds)
+            await transaction(AppVar.knex(), async (tx) => {
+                await AppVar.query(tx).deleteById(APP_VAR_KEYS.GDRIVE_ACCESS_TOKEN)
+                await AppVar.query(tx).insert({
+                    key: APP_VAR_KEYS.GDRIVE_ACCESS_TOKEN,
+                    value: JSON.stringify(creds)
+                })
             })
+            await ctx.reply(__('gdrive.ok'))
             await ctx.scene.enter(SCENE.MAIN)
         } catch (e) {
             await ctx.reply(`<code>${e}</code>`, Extra.HTML(true) as ExtraReplyMessage)
-            await ctx.scene.enter(SCENE.MAIN)
             throw e
         }
     })
