cf096c6700ae2b05322d1cfe76afe83d9930e423
BANO.notIT <bano.notit@yandex.ru>
2019-12-20 07:43:01 +0300

    db: merge appvars migrations

--- migrations/20191118012737_app_vars.js
+++ migrations/20191118012737_app_vars.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('app_vars', (t) => {
         t.primary(['key'])
         t.string('key')
-        t.string('value').notNullable()
+        t.string('value', 1000).notNullable()
     })
 }
 
