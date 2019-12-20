ca247fdb6f876bdc2b735099d212f55374f704c2
BANO.notIT <bano.notit@yandex.ru>
2019-12-20 05:52:16 +0300

    feat: add user logging in sentry

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -1,11 +1,25 @@
 import { ContextMessageUpdate, Middleware } from 'telegraf'
 import User from '../models/User'
+import { captureException, configureScope } from '@sentry/node'
 
 async function attachUser(ctx, next) {
     ctx.user = await User.query()
         .findById(ctx.from.id)
         .eager('team')
-    await next()
+
+    try {
+        await next()
+    } catch (e) {
+        configureScope(scope => {
+            scope.setUser({
+                id: ctx.user.$id(),
+                username: ctx.from.username
+            })
+            scope.setExtras(ctx)
+
+            captureException(e)
+        })
+    }
 }
 
 export default attachUser as Middleware<ContextMessageUpdate>
