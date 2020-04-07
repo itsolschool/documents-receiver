9657fdf8cc5ae2aa0bd774d0f5bfb2e9c817fc5d
BANO.notIT <bano.notit@yandex.ru>
2020-04-08 01:25:25 +0300

    fix: specify attachedTime field in Document Model

--- src/models/Document.ts
+++ src/models/Document.ts
@@ -28,6 +28,7 @@ export default class Document extends Model {
     // https://vincit.github.io/objection.js/api/model/static-properties.html#static-uselimitinfirst
     static useLimitInFirst = true
 
+    attachedTime!: Date
     documentId!: number
     team!: Team
     milestone!: number
